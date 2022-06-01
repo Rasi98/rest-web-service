029b075a5409a1f6fd0a2e7e83035be84417f5c6
Lakshan Rasingolla <rwmlakshan@gmail.com>
2022-06-01 21:01:17 +0530

    -Frontend  -completed

--- src/main/java/com/example/RESTwebservice/CircleAreaController.java
+++ src/main/java/com/example/RESTwebservice/CircleAreaController.java
@@ -1,5 +1,6 @@
 package com.example.RESTwebservice;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CircleAreaController {
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/area")
-    public CircleArea circleArea(@RequestParam(value = "radius",defaultValue = "0") double radius){
+    public CircleArea circleArea(@RequestParam(value = "radius", defaultValue = "0") double radius) {
         return new CircleArea(radius);
     }
 }
